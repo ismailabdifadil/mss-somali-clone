cc37759ce0ca98fe8f1262eac188bf4709ca04dd
Ismail <ismail.haabare@gmail.com>
2024-08-31 10:23:30 +0300

    Fix modal error

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Competitors from '../components/competitors/Competitors';
 import Footer from '../components/Footer/Footer';
+import VoteModal from '../components/modal/VoteModal';
 import Time from '../components/Timer/Time';
 const HomePage = () => {
   return (
     <div>
       <Time />
       <Competitors isHome={true} />
-      <Footer/>
+      <Footer />
+      <VoteModal />
     </div>
   );
 };
